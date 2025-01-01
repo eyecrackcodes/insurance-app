4f938372071279059cfe9780f05b4edc3bb49cdf
eyecrackcodes <eyecrackcodes@gmail.com>
2025-01-01 17:31:27 -0600

    Fixed host and port for Heroku deployment

--- app.py
+++ app.py
@@ -906,6 +906,6 @@ def get_state_list():
     ]
 
 
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
 
-if __name__ == '__main__':
-    app.run(debug=True)
